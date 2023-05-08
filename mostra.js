2872fa1ad8f118dfbd673621dd8a5f1656245ed9
Zelliwt <131499234+Zelliwt@users.noreply.github.com>
2023-05-07 23:59:22 -0300

    Add files via upload

---
+++ mostra.js
@@ -0,0 +1,17 @@
+function Mostra() {
+    var x = document.getElementById("input");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
+  
+function Mostra2() {
+    var x = document.getElementById("input2");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
